caedc38cd9a08fc37c9add31fc7c89c3bdcb22fe
karanjeetsingh <karanjeetsingh@live.in>
2015-11-19 11:53:12 -0600

    Checkin

--- src/main/java/com/interviewalgo/strings/TwoSum.java
+++ src/main/java/com/interviewalgo/strings/TwoSum.java
@@ -1,5 +1,8 @@
 package com.interviewalgo.strings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers, find two numbers such that they add up to a
  * specific target number.
@@ -19,8 +22,11 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] numbers = { 2, 7, 11, 13 };
         int target = 9;
-        int[] ret = twoSum_1(numbers, target);
-        System.out.println(ret[0] + ", " + ret[1]);
+        int[] ret1 = twoSum_1(numbers, target);
+        System.out.println(ret1[0] + ", " + ret1[1]);
+
+        int[] ret2 = twoSum_2(numbers, target);
+        System.out.println(ret2[0] + ", " + ret2[1]);
     }
 
     /**
@@ -44,6 +50,31 @@ public class TwoSum {
             }
         }
 
+        return ret;
+    }
+
+    /**
+     * Use hashmap to save the target value. The complexity depends upon the
+     * input output of hashmap, which is usually O(1). So, the total time
+     * complexity is O(n).
+     * 
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public static int[] twoSum_2(int[] numbers, int target) {
+        int[] ret = new int[2];
+        Map<Integer, Integer> numbersMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbersMap.containsKey(numbers[i])) {
+                int index = numbersMap.get(numbers[i]);
+                ret[0] = index + 1;
+                ret[1] = i + 1;
+                break;
+            } else {
+                numbersMap.put(target - numbers[i], i);
+            }
+        }
         return ret;
     }
 }
